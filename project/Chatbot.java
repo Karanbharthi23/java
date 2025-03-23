5c66aefbf0034c2a2fd9f1c99c0255c0875392c9
Karan Goswami <goswamikaran5679@gmail.com>
2025-03-23 07:42:16 +0000

    EDITED SECTION

--- project/Chatbot.java
+++ project/Chatbot.java
@@ -8,23 +8,26 @@ public class Chatbot
         }
         public String response(String userquery)
         {
-            if(userquery.toLowerCase().contains("hello"))
+            if(userquery.toLowerCase().contains("hello")||userquery.toLowerCase().contains("hyy"))
             return "hello "+username+" !";
-            else if(userquery.toLowerCase().contains("how are you"))
+            else if(userquery.toLowerCase().contains("how are you")||userquery.toLowerCase().contains("how r u?")||userquery.toLowerCase().contains("how are u?"))
             return "i am fine";
-            else if(userquery.toLowerCase().contains("who is your devloper?"))
+            else if(userquery.toLowerCase().contains("who is your devloper?")||userquery.toLowerCase().contains("devloper"))
             return "Karan Goswami ";
-            else 
-            return "invaliid input entered";
+            else if(userquery.toLowerCase().contains("your one friend name")||userquery.toLowerCase().contains("brother"))
+            return "Raj Chauhan";
+            else
+            return "invalid enter";
         }
         public static void main(String[] args)
         {
             Scanner scanner=new Scanner(System.in);
+            System.out.println("Enter your username ");
             String username=scanner.nextLine();
             Chatbot chatbot=new Chatbot(username);
             while(true)
             {
-                System.out.println("You ");
+                System.out.println("Your queries pass here .. ");
                 String userquery=scanner.nextLine();
                 String response=chatbot.response(userquery);
                 System.out.println("Chatbot "+response);
